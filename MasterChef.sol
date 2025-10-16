b74a61a89888a05f3a9fc0c12ef9216a79be7e22
Fety <warpfinanceeth@gmail.com>
2025-10-15 17:54:09 -0700

    update

--- MasterChef.sol
+++ MasterChef.sol
@@ -1,8 +1,71 @@
-// Sources flattened with hardhat v2.0.11 https://hardhat.org
-
-// File @boringcrypto/boring-solidity/contracts/libraries/BoringMath.sol@v1.0.4
-
 // SPDX-License-Identifier: MIT
+// File: ReentrancyGuard.sol
+
+
+
+pragma solidity >=0.6.0 <0.8.0;
+
+/**
+ * @dev Contract module that helps prevent reentrant calls to a function.
+ *
+ * Inheriting from `ReentrancyGuard` will make the {nonReentrant} modifier
+ * available, which can be applied to functions to make sure there are no nested
+ * (reentrant) calls to them.
+ *
+ * Note that because there is a single `nonReentrant` guard, functions marked as
+ * `nonReentrant` may not call one another. This can be worked around by making
+ * those functions `private`, and then adding `external` `nonReentrant` entry
+ * points to them.
+ *
+ * TIP: If you would like to learn more about reentrancy and alternative ways
+ * to protect against it, check out our blog post
+ * https://blog.openzeppelin.com/reentrancy-after-istanbul/[Reentrancy After Istanbul].
+ */
+abstract contract ReentrancyGuard {
+    // Booleans are more expensive than uint256 or any type that takes up a full
+    // word because each write operation emits an extra SLOAD to first read the
+    // slot's contents, replace the bits taken up by the boolean, and then write
+    // back. This is the compiler's defense against contract upgrades and
+    // pointer aliasing, and it cannot be disabled.
+
+    // The values being non-zero value makes deployment a bit more expensive,
+    // but in exchange the refund on every call to nonReentrant will be lower in
+    // amount. Since refunds are capped to a percentage of the total
+    // transaction's gas, it is best to keep them low in cases like this one, to
+    // increase the likelihood of the full refund coming into effect.
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+
+    uint256 private _status;
+
+    constructor () internal {
+        _status = _NOT_ENTERED;
+    }
+
+    /**
+     * @dev Prevents a contract from calling itself, directly or indirectly.
+     * Calling a `nonReentrant` function from another `nonReentrant`
+     * function is not supported. It is possible to prevent this from happening
+     * by making the `nonReentrant` function external, and make it call a
+     * `private` function that does the actual work.
+     */
+    modifier nonReentrant() {
+        // On the first call to nonReentrant, _notEntered will be true
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+
+        // Any calls to nonReentrant after this point will fail
+        _status = _ENTERED;
+
+        _;
+
+        // By storing the original value once again, a refund is triggered (see
+        // https://eips.ethereum.org/EIPS/eip-2200)
+        _status = _NOT_ENTERED;
+    }
+}
+// File: Masterchef.sol
+
+
 
 pragma solidity 0.6.12;
 // a library for performing overflow-safe math, updated with awesomeness from of DappHub (https://github.com/dapphub/ds-math)
@@ -95,6 +158,7 @@ library BoringERC20 {
 
 // P1 - P3: OK
 pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
 // solhint-disable avoid-low-level-calls
 // T1 - T4: OK
 contract BaseBoringBatchable {
@@ -320,7 +384,7 @@ interface IRewarder {
 // File contracts/interfaces/IMasterChef.sol
 
 pragma solidity 0.6.12;
-pragma experimental ABIEncoderV2;
+
 
 interface IMasterChef {
     using BoringERC20 for IERC20;
@@ -345,7 +409,7 @@ interface IMasterChef {
 // File contracts/Masterchef.sol
 
 pragma solidity 0.6.12;
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
 
 /// @notice The (older) MasterChef contract gives out a constant number of SUSHI tokens per block.
 /// It is the only address with minting rights for SUSHI.
